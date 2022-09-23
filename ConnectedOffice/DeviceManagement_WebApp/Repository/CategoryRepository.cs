5c46907c74df60464c0047c029a28e27c2e957a6
Ruan <ruanc.burger@gmail.com>
2022-09-23 16:47:23 +0200

    Added Category Repository and Interface

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        //Create additional methods here
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        //Add interface definitions of any additional methods here.
+    }
+}
