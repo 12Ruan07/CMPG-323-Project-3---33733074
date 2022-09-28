3003378b3a1d94b2e1841f3ff827f0fa0590d186
Ruan <ruanc.burger@gmail.com>
2022-09-28 16:10:29 +0200

    Added new methods and comments to zone controller

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
@@ -21,34 +21,138 @@ namespace DeviceManagement_WebApp.Controllers
         }
 
         // GET: Zones
+        // Shows all of the zones from the database
        public async Task<IActionResult> Index()
         {
             return View(_zoneRepository.GetAll());
         }
 
         // GET: Zones/Details/5
-        
+        // Shows details of a singular zone with specified id
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = _zoneRepository.GetById(id);
+               
+            if (zone == null)
+            {
+                return NotFound();
+            }
+
+            return View(zone);
+        }
+
 
         // GET: Zones/Create
-       
+        // Returns view of zones
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         // POST: Zones/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-       
+        // Adds a new zone to the database, user has to enter zone name and description
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ZoneId,ZoneName,ZoneDescription,DateCreated")] Zone zone)
+        {
+            zone.ZoneId = Guid.NewGuid();
+            _zoneRepository.Add(zone);
+            _zoneRepository.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
 
         // GET: Zones/Edit/5
-        
+        // Returns view of zones
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = _zoneRepository.GetById(id);
+            if (zone == null)
+            {
+                return NotFound();
+            }
+            return View(zone);
+        }
 
         // POST: Zones/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        
+        // Updates an existing zone, user is able to change zone name and/or description
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ZoneId,ZoneName,ZoneDescription,DateCreated")] Zone zone)
+        {
+            if (id != zone.ZoneId)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _zoneRepository.Update(zone);
+                _zoneRepository.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ZoneExists(zone.ZoneId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
 
         // GET: Zones/Delete/5
-       
-        
+        // Returns view of zones
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = _zoneRepository.GetById(id);
+            if (zone == null)
+            {
+                return NotFound();
+            }
+
+            return View(zone);
+        }
+
+
 
         // POST: Zones/Delete/5
-        
+        // Deletes an existing zone from the database
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var zone = _zoneRepository.GetById(id);
+            _zoneRepository.Remove(zone);
+            _zoneRepository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // CheckExists
+        // Checks if the zone with specified id exists in the database
+        private bool ZoneExists(Guid id)
+        {
+            return _zoneRepository.GetById(id) != null;
+        }
+
     }
 }
